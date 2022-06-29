81c75894b5ab348a921102073af896dd594a0554
Nguyễn Văn Vương <nguyentuevuong@gmail.com>
2022-06-29 21:55:32 +0700

    initial first file

---
+++ src/main/java/com/nittsu/kinjiro/KinjiroApplication.java
@@ -0,0 +1,13 @@
+package com.nittsu.kinjiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinjiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinjiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nittsu/kinjiro/KinjiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nittsu.kinjiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinjiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
